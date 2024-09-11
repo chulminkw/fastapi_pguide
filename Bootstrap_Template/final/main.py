5bc3eb9d79a3a69d7945c8777ae2daa1b9e48e7e
chulminkw <55954384+chulminkw@users.noreply.github.com>
2024-09-11 14:27:46 +0900

    Add files via upload

---
+++ Bootstrap_Template/final/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# jinja2 Template 생성. 인자로 directory 입력
+templates = Jinja2Templates(directory="templates")
+
+class Item(BaseModel):
+    name: str
+    description: str
+
+@app.get("/all_items", response_class=HTMLResponse)
+async def read_all_items(request: Request):
+    all_items = [Item(name="테스트_상품명_" +str(i), 
+                      description="테스트 내용입니다. 인덱스는 " + str(i)) for i in range(5) ]
+    print("all_items:", all_items)
+    return templates.TemplateResponse(
+        request=request, 
+        #name="index_no_include.html",
+        #name="index_include.html",
+        name="index.html", 
+        context={"all_items": all_items}
+    )
