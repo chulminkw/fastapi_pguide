a4c46667db6f86ef3f33ea75049eda1b00011b8d
chulminkw <55954384+chulminkw@users.noreply.github.com>
2024-10-02 18:00:12 +0900

    Add files via upload

--- Requests/main_rbody.py
+++ Requests/main_rbody.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from typing import Optional, Annotated
-from starlette.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -12,8 +10,10 @@ class Item(BaseModel):
     description: str | None = None
     #description: Optional[str] = None
     price: float
-    #tax: float | None =  None
-    tax: Optional[float] = None
+    tax: float | None = None
+    #tax: Optional[float] = None
+
+    
 
 #수행 함수의 인자로 Pydantic model이 입력되면 Json 형태의 Request Body 처리
 @app.post("/items")
@@ -26,19 +26,22 @@ async def create_item(item: Item):
 # Request Body의 Pydantic model 값을 Access하여 로직 처리
 @app.post("/items_tax/")
 async def create_item_tax(item: Item):
-    item_dict = item.model_dump() #item.dict()가 deprecated됨.
+    item_dict = item.model_dump()
     print("#### item_dict:", item_dict)
     if item.tax:
         price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})   
-    return item_dict
+        item_dict.update({"price_with_tax": price_with_tax})
+    
+    return item_dict   
 
 # Path, Query, Request Body 모두 함께 적용. 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item, q: str | None = None):
     result = {"item_id": item_id, **item.model_dump()}
+    
     if q:
         result.update({"q": q})
+    print("#### result:", result)
     return result
 
 class User(BaseModel):
@@ -46,17 +49,12 @@ class User(BaseModel):
     full_name: str | None = None
     #full_name: Optional[str] = None
 
+
 # 여러개의 request body parameter 처리. 
 # json 데이터의 이름값과 수행함수의 인자명이 같아야 함.  
 @app.put("/items_mt/{item_id}")
 async def update_item_mt(item_id: int, item: Item, user: User):
     results = {"item_id": item_id, "item": item, "user": user}
-    return results
-
-# 개별 json 값을 포함한 경우, Body()를 이용함. 
-@app.put("/items_sn/{item_id}")
-async def update_item_sn(item_id: int, item: Item, user: User, importance: int = Body()):
-#async def update_item_sn(item_id: int, item: Item, user: User, importance: Annotated[int, Body] = None):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    print("results:", results)
     return results
 
